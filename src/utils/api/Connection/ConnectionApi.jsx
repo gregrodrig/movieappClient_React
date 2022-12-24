de9b0ad6bac3157472c73d251df70396c42b1588
greg3391 <gregory3391@gmail.com>
2022-12-24 12:19:41 +0100

    adding custom environment variables and env.template

--- src/utils/api/Connection/ConnectionApi.jsx
+++ src/utils/api/Connection/ConnectionApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: process.env.REACT_APP_BASEURL,
   timeout: 2000,
   headers: { "X-Custom-Header": "foobar" },
 });
